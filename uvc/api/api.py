144ad0ae81a30be58bd5c83b9760e4ffb10e647d
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2012-08-22 12:13:57 +0000

    Creating Space

---
+++ uvc/api/api.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2007-2011 NovaReto GmbH
+# cklinger@novareto.de 
+
+from uvc.api import log 
+from zope import interface
+
+try:
+    import grok
+    GROK = True
+except:
+    GROK = False
+
+log('Running nva.tabsuche in GROK Environment --> %s' % GROK)
+
+
+if GROK:
+    from uvcsite import Page
+    from uvcsite import Form
+    import grok
+    from zope.security.interfaces import IPrincipal
+
+    def get_princiapl(context, request):
+        return request.principal
+
+else:
+    from five.grok import View 
+    from five import grok
+    from zope.publisher.publish import mapply
+    from zeam.form.plone import Form
+    from zope.component import getMultiAdapter
+    from AccessControl.interfaces import IUser as IPrincipal
+    from grokcore.layout import Layout, Page
+
+    grok.templatedir('templates')
+
+    class Layout(Layout):
+        grok.name('layout')
+        grok.context(interface.Interface)
+
+
+    def get_princiapl(context, request):
+        portal_state = getMultiAdapter((context, request), name="plone_portal_state")
+        return portal_state.member().getUser()
